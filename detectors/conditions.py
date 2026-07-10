2a4e2f4e97e7b17fc2ed910716bce22ce0b75c56
DongHa,Lee <qaqa1321@gmail.com>
2026-07-10 13:51:50 +0900

    feat: syn, ack, fin flood 동일조건 함수 추가

---
+++ detectors/conditions.py
@@ -0,0 +1,27 @@
+from engine import Flow
+
+def conditions(flow: Flow):
+    """
+    조건값
+    """
+
+    if flow.protocol != "TCP":
+        return None
+    
+    recent_packets = flow.get_packets(seconds=10)
+    packet_count = len(recent_packets)
+
+    if packet_count == 0 :
+        return None
+    
+    recent_duration = (recent_packets[-1].timestamp - recent_packets[0].timestamp)
+
+    if recent_duration < 2:
+        return None
+
+    pps = packet_count / recent_duration
+
+    if pps < 300:
+        return None
+
+    return {"packet_count": packet_count,"pps": pps,"duration": recent_duration}
