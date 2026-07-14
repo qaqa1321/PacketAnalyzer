02d66008c2d75271ae1f8a2423c884ee6f42b251
CHR9705 <changhoryu97@gmail.com>
2026-07-14 10:54:59 +0900

    feat: settings 페이지 경로 추가

--- index.py
+++ index.py
@@ -4,5 +4,6 @@ pg = st.navigation([
         st.Page('webpages/pages/home.py', title='🏠 Home'),
         st.Page('webpages/pages/details.py', title='📈 Details'),
         st.Page('webpages/pages/ipcountry.py', title='🗺️ IP Country'),
+        st.Page('webpages/pages/settings.py', title='settings'),
     ])
 pg.run()
